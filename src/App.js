4e3dd39cff4347ede40d4925fc4bd1cf4fd3094e
JaeWoogie <60237088+JaeWoogie@users.noreply.github.com>
2022-10-17 23:29:34 -0400

    React for fun

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense';
 
 function App() {
   const expenses = [
@@ -26,13 +27,13 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense => {
+
+  }
+
   return (
     <div>
-      <h1 className='App-header'>React for Fun!!</h1>
-      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} /> */}
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
